c93ad066b36a66842852749c1c5bc077d48550b7
chenxin777 <chenxinfang77@gmail.com>
2024-06-27 20:44:54 +0800

    上线前的优化

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,2 +1,2 @@
-export const SYSTEM_LOGO = "/icons/play.jpg"
+export const SYSTEM_LOGO = "/icons/logo.png"
 export const GITHUB_URL = "https://github.com/chenxin777"
--- src/plugins/globalRequest.ts
+++ src/plugins/globalRequest.ts
@@ -1,44 +1,44 @@
 import {extend} from 'umi-request';
-import {history} from "umi";
-import {stringify} from "querystring";
-import {message} from "antd";
+import {history} from 'umi';
+import {stringify} from 'querystring';
+import {message} from 'antd';
 
 const request = extend({
-    credentials: 'include', // 默认请求是否带上cookie
-    prefix: process.env.NODE_ENV === 'production' ? 'http://localhost:8080' : undefined,
-
+	credentials: 'include', // 默认请求是否带上cookie
+	prefix: process.env.NODE_ENV === 'production' ? 'http://8.140.27.137' : 'http://localhost:8080',
+	
 });
 
 request.interceptors.request.use((url, options): any => {
-    console.log(`do request url = ${url}`)
-    return {
-        options: {
-            ...options,
-            headers: {},
-        }
-    };
+	console.log(`do request url = ${url}`);
+	return {
+		options: {
+			...options,
+			headers: {},
+		}
+	};
 });
 
 request.interceptors.response.use(async (response, options): Promise<any> => {
-    const res = await response.clone().json();
-    if (res.code === 0) {
-        return res.data;
-    }
-    // 未登录
-    if (res.code === 40100) {
-        message.error('请先登录');
-        history.replace(
-            {
-                pathname: 'user/login',
-                search: stringify({
-                    redirect: location.pathname,
-                })
-            }
-        )
-    } else {
-        message.error(res.description);
-    }
-    return res.data;
+	const res = await response.clone().json();
+	if (res.code === 0) {
+		return res.data;
+	}
+	// 未登录
+	if (res.code === 40100) {
+		message.error('请先登录');
+		history.replace(
+			{
+				pathname: 'user/login',
+				search: stringify({
+					redirect: location.pathname,
+				})
+			}
+		);
+	} else {
+		message.error(res.description);
+	}
+	return res.data;
 });
 
 export default request;
